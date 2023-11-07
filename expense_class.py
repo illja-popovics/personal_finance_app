44e56abd03534ae1f9a2c0a3fccda429e6475caa
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2023-11-07 08:23:15 +0100

    commit

--- expense_class.py
+++ expense_class.py
@@ -1,10 +1,19 @@
 from datetime import datetime
+import json
 
 class Expense:
-    def __init__(self, amount, category, date=None) -> None:
+    def __init__(self, amount, category, user, month=None) -> None:
+        if int(amount) >= 0:
+            raise ValueError("Amount must be a positive number")
+        if category not in ["Rent", "Utilities", "", "", "", "", "", ""]:
+            raise ValueError("Category must be one of these:")
+        if not 1 <= month <=12:  
+            raise ValueError("Month must be a datetime.month object or None")
+        
         self.amount = amount
         self.category = category
-        self.date = date
+        self.month = month
+        self.user = user
     
     def edit_expense(self, new_data):
         pass
@@ -13,20 +22,49 @@ class Expense:
     def from_dict(cls, data):
         return cls(data['amount'], data['category'], data['date'])
 
-class Income:
-    pass
+    def __str__(self):
+        return f"Amount: {self.amount}, Category: {self.category}, Month: {self.month}"
 
-class Account:
-    def __init__(self, month : datetime.month, total_amount=0 ):
+
+class Income:
+    def __init__(self, amount, category, user, month=None) -> None:
+        if int(amount) <= 0:
+            raise ValueError("Amount must be a positive number")
+        if category not in ["Salary", "", ]:
+            raise ValueError("Category must be one of these:")
+        if month is not None and not isinstance(month, datetime.month):
+            raise ValueError("Month must be a datetime.month object or None")
+        
+        self.amount = amount
+        self.category = category
         self.month = month
+        self.user = user
+
+    def edit_income(self, new_data):
+        pass
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data['amount'], data['category'], data['date'])
+
+
+class Account_per_month:
+    def __init__(self, month : datetime.month):
         self.month = month
-        self.total_amount = total_amount
+        self.total_amount = 0
         self.registry = []
     
-    def add_expense(self, expense:Expense):
+    def add_expense(self, expense: Expense):
         self.total_amount += expense.amount
-        self.registry.append({'amount':expense.amount, "category": expense.category, "date": expense.date})
+        self.registry.append({'amount':expense.amount, "category": expense.category, "month": expense.month, "user": expense.user})
 
+    def add_income(self, income: Income):
+        self.total_amount += int(income.amount)
+        self.registry.append({'amount':income.amount, "category": income.category, "month": income.month, "user": income.user})
+
+
+    def save_registry(self, file_path):
+        pass 
 
 
 
---
+++ main.py
@@ -0,0 +1,9 @@
+from expense_class import Expense, Income, Account_per_month
+
+oct = Account_per_month(10)
+
+exp1 = Expense(-100.50, "Rent", "Illja", 10)
+
+oct.add_expense(exp1)
+print(oct.registry)
+print(oct.total_amount)
