2360e3a8a1e6cb4ad65b86c10e11d5e2c60100bf
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2023-11-08 11:47:21 +0100

    new items

--- expense_classes.py
+++ expense_classes.py
@@ -7,10 +7,11 @@ from fields import *
 class Expense:
     def __init__(self, amount, category, user, month=None) -> None:
     
-        self.amount : Amount = amount
-        self.category : ExpenseCategory = category
-        self.month: Month = month
-        self.user: User = user
+        self.amount = Amount(amount)
+        self.category : ExpenseCategory = ExpenseCategory(category)
+        if month != None:
+            self.month = Month(month)
+        self.user = User(user)
     
     def edit_expense(self, new_data):
         # need to write a method 
@@ -27,10 +28,10 @@ class Expense:
 class Income:
     def __init__(self, amount, category, user, month=None) -> None:
         
-        self.amount: Amount  = amount
-        self.category: IncomeCategory = category
-        self.month: Month = month
-        self.user: User = user
+        self.amount = Amount(amount)
+        self.category  = IncomeCategory(category)
+        self.month = Month(month)
+        self.user = User(user)
 
     def edit_income(self, new_data):
         # need to write a method 
@@ -44,12 +45,12 @@ class Income:
         return f"Amount: {self.amount}, Category: {self.category}, Month: {self.month}"
 
 class Registry(UserList):
-    def __init__(self, data, month):
+    def __init__(self, data, month : Month):
         super().__init__(data)
-        self.month: Month = month
+        self.month = month
     def to_dict(self):
         return {
-            "month": self.month,
+            "month": self.month.value,
             "registry" : self.data
         }
     def save_to_json(self, file_path):
@@ -59,21 +60,46 @@ class Registry(UserList):
 
 class Account_per_month:
     def __init__(self, month):
-        self.month : Month = month
-        self.total_amount = 0
+        self.month = Month(month)
         self.registry = Registry([],month)
     
     def add_expense(self, expense: Expense):
-        self.total_amount -= expense.amount
-        self.registry.append({'amount':expense.amount, "category": expense.category, "month": expense.month, "user": expense.user})
+        if expense.month == None:
+                expense.month = self.month
+        if expense.month.value != self.month.value:
+            raise ValueError("Expense from a different month cannot be added to the account")
+        self.registry.append({
+             "type": "expense",
+             "amount": expense.amount.value,
+             "category": expense.category.value,
+             "month": expense.month.value,
+             "user": expense.user.value})
+        
 
     def add_income(self, income: Income):
-        self.total_amount += int(income.amount)
-        self.registry.append({'amount':income.amount, "category": income.category, "month": income.month, "user": income.user})
+        if income.month == None:
+                income.month = self.month
+        if income.month.value != self.month.value:
+            raise ValueError("Income from a different month cannot be added to the account")
+        self.registry.append({
+             "type": "income",
+             "amount": income.amount.value,
+             "category": income.category.value,
+             "month": income.month.value,
+             "user": income.user.value})
 
 
     def save_registry(self, file_path):
         return self.registry.save_to_json(file_path)
+    
+    def balance(self):
+        res = 0
+        for item in self.registry:
+            if item["type"] == "income":
+                res += item["amount"]
+            elif item["type"] == "expense":
+                res -= item["amount"]
+        return res
 
 
 
